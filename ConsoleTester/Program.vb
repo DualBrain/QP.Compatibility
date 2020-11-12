c6697299fbbc2497115060937f03168d464c8a55
Cory Smith <dualbrain@msn.com>
2020-11-12 13:13:07 -0600

    Updated to .NET 5.

---
+++ ConsoleTester/Program.vb
@@ -0,0 +1,15 @@
+Imports System
+
+Module Program
+
+  Sub Main() 'args As String())
+
+    Console.WriteLine("Hello World!")
+
+    Console.WriteLine(QP.Dos.DCount("C:\Audio"))
+
+    Console.ReadLine()
+
+  End Sub
+
+End Module
